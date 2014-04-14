a3f228061b8487c8bef2b260897ca23ff9118c89
sararose <sara.rose.schuster@gmail.com>
2014-04-14 19:11:14 -0400

    added empty methods to palace festival actions

--- src/model/actions/Action.java
+++ src/model/actions/Action.java
@@ -1,7 +1,21 @@
 package model.actions;
 
+import model.GameModel;
+import model.actions.serialization.JsonObject;
+
 /**
  * Created by Owner on 4/14/14.
  */
 public abstract class Action {
+
+    public abstract boolean tryAction(GameModel game);
+
+    public abstract boolean doAction(GameModel game);
+
+    public abstract String serialize(GameModel game);
+
+    public abstract Action restore(JsonObject actionToRestore);
+
+    public abstract int getActionID();
+
 }
--- src/model/actions/palacefestival/BeginPalaceFestival.java
+++ src/model/actions/palacefestival/BeginPalaceFestival.java
@@ -1,9 +1,50 @@
 package model.actions.palacefestival;
 
+import model.GameModel;
 import model.actions.Action;
+import model.actions.serialization.JsonObject;
+import model.board.Location;
+import model.palacefestival.PalaceCard;
+
+import java.util.ArrayList;
 
 /**
  * Created by Baker on 4/14/2014.
  */
 public class BeginPalaceFestival extends Action {
+
+    private int palaceValueForFestival;
+    private Location palaceLocation;
+    private ArrayList<PalaceCard> cardsBidded;
+
+    @Override
+    public boolean tryAction(GameModel game) {
+        // TODO: Sara
+        throw new UnsupportedOperationException("Tell Sara to implement me!");
+    }
+
+    @Override
+    public boolean doAction(GameModel game) {
+        // TODO: Sara
+        throw new UnsupportedOperationException("Tell Sara to implement me!");
+    }
+
+    @Override
+    public String serialize(GameModel game) {
+        // TODO: Sara
+        throw new UnsupportedOperationException("Tell Sara to implement me!");
+    }
+
+    @Override
+    public Action restore(JsonObject actionToRestore) {
+        // TODO: Sara
+        throw new UnsupportedOperationException("Tell Sara to implement me!");
+    }
+
+    @Override
+    public int getActionID() {
+        // TODO: Sara
+        throw new UnsupportedOperationException("Tell Sara to implement me!");
+    }
+
 }
