1e94fe50511f0bcc3447cc7f719450ccc559deb2
sararose <sara.rose.schuster@gmail.com>
2014-04-14 18:35:29 -0400

    added empty methods to part of player subsystem

--- src/model/player/TurnTracker.java
+++ src/model/player/TurnTracker.java
@@ -4,4 +4,25 @@ package model.player;
  * Created by Baker on 4/14/2014.
  */
 public class TurnTracker<T> {
+
+    public void addPlayer(T player) {
+        // TODO:sara
+        throw new UnsupportedOperationException();
+    }
+
+    public void removePlayer(T player) {
+        // TODO:sara
+        throw new UnsupportedOperationException();
+    }
+
+    public T getCurrentPlayer() {
+        // TODO:sara
+        throw new UnsupportedOperationException();
+    }
+
+    public void advanceTurn() {
+        // TODO:sara
+        throw new UnsupportedOperationException();
+    }
+
 }
