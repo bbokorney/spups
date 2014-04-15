48ee132208e5e26b81afcf10515293bcd73417fc
unknown <jonathan.howe.yu@gmail.com>
2014-04-14 20:29:50 -0400

    Added skeleton to turn and GameModel

--- src/model/turn/Turn.java
+++ src/model/turn/Turn.java
@@ -3,5 +3,8 @@ package model.turn;
 /**
  * Created by Baker on 4/14/2014.
  */
-public class Turn {
+public abstract class Turn {
+    public abstract boolean endTurn();
+    public abstract int getAvailableAPPoints(boolean isLandTileAction);
+    public abstract boolean hasUsedActionToken();
 }
