ccf53b1c3d78052bf7d7fc88496c129bff6d6290
sararose <sara.rose.schuster@gmail.com>
2014-04-15 16:46:36 -0400

    created association class between Java Game and Palace Festival, called it GameModel

--- src/model/JavaGameModel.java
+++ src/model/JavaGameModel.java
@@ -12,79 +12,79 @@ import java.util.List;
 /**
  * Created by Baker on 4/14/2014.
  */
-public class JavaGameModel extends GameModel{
+public class JavaGameModel {
 
-    @Override
+    
     int getCount(SharedResourceType res) {
         return 0;
     }
 
-    @Override
+    
     void useResource(SharedResourceType res) {
 
     }
 
-    @Override
+    
     int getCount(JavaPlayerResourceType res) {
         return 0;
     }
 
-    @Override
+    
     void useResource(JavaPlayerResourceType res) {
 
     }
 
-    @Override
+    
     int getAvailableAPPoints(boolean isLandTileAction) {
         return 0;
     }
 
-    @Override
+    
     boolean hasUsedActionToken() {
         return false;
     }
 
-    @Override
+    
     void advanceTurn() {
 
     }
 
-    @Override
+    
     boolean canAdvanceTurn() {
         return false;
     }
 
-    @Override
+    
     void beginFinalRound() {
 
     }
 
-    @Override
+    
     JavaPlayer getCurrentPlayer() {
         return null;
     }
 
-    @Override
+    
     boolean placeTopTileComponent(Location loc, TileComponent tile) {
         return false;
     }
 
-    @Override
+    
     void getTopTileComponent(Location loc) {
 
     }
 
-    @Override
+    
     String getName() {
         return null;
     }
 
-    @Override
+    
     void adjustScore(int score) {
 
     }
 
-    @Override
+    
     List<Developer> getDevelopers() {
         return null;
     }
