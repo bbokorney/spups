f82c46da48a89de9eae16caf7c16b534a57cea59
unknown <jonathan.howe.yu@gmail.com>
2014-04-14 21:01:35 -0400

    Added skeleton for KeyListener subsystem

--- src/controller/keylistener/InternalListener.java
+++ src/controller/keylistener/InternalListener.java
@@ -1,7 +1,23 @@
 package controller.keylistener;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 /**
  * Created by Baker on 4/14/2014.
  */
 public class InternalListener {
+    private int keyCode;
+    private Funktor method;
+
+    public InternalListener(int keyCode, Funktor method) {
+        this.keyCode = keyCode;
+        this.method = method;
+    }
+
+    public void actionPerformed(KeyEvent event) {
+        if(event.getKeyCode() == keyCode) {
+            method.call();
+        }
+    }
 }
--- src/controller/keylistener/KeyListener.java
+++ src/controller/keylistener/KeyListener.java
@@ -1,7 +1,65 @@
-package controller.keylistener.potentialuistates.keylistener
+package controller.keylistener;
+
+import controller.keylistener.InternalListener;
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Baker on 4/14/2014.
  */
-public class KeyListener {
+public class KeyListener implements java.awt.event.KeyListener{
+
+    private List<InternalListener> temporary;
+    private List<InternalListener> persistent;
+    private boolean persistentEnabled;
+
+    public KeyListener() {
+        persistentEnabled = true;
+        temporary = new ArrayList<InternalListener>();
+        persistent = new ArrayList<InternalListener>();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        for(InternalListener i : temporary) {
+            i.actionPerformed(e);
+        }
+        if(persistentEnabled) {
+            for(InternalListener i : persistent) {
+                i.actionPerformed(e);
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    public void replaceTemporaryListener(List<InternalListener> listeners) {
+        temporary = listeners;
+    }
+
+    public void addPersistentListener(InternalListener listener) {
+        persistent.add(listener);
+    }
+
+    public void clearPersistentListeners() {
+        persistent.clear();
+    }
+
+    public void disablePersistentListeners() {
+        persistentEnabled = false;
+    }
+
+    public void enablePersistentListeners() {
+        persistentEnabled = true;
+    }
 }
