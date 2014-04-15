56112d927c59a94d9ddd807788f8defbb4a9f93f
Baker Bokorney <bbokorney@gmail.com>
2014-04-14 21:06:13 -0400

    Fixed missing package error.

--- src/model/board/Directions.java
+++ src/model/board/Directions.java
@@ -1,3 +1,4 @@
+package model.board;
 /** The directions of movement in the game (clockwise).
  * This will be used when creating the list of movements
  * from origin to this HexLocation. Note that this convention
