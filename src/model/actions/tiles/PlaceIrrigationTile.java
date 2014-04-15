101bcca55390017d226475d63609bf009319060c
Meghan King <meghantking67@gmail.com>
2014-04-14 23:55:26 -0400

    Merged code for all the tile placement actions

--- src/model/actions/tiles/PlaceIrrigationTile.java
+++ src/model/actions/tiles/PlaceIrrigationTile.java
@@ -1,7 +1,131 @@
 package model.actions.tiles;
 
+import model.GameModel;
+import model.actions.Action;
+import model.actions.ActionResult;
+import model.actions.serialization.JsonObject;
+import model.board.Location;
+
 /**
- * Created by Baker on 4/14/2014.
+ * Created by idinamenzel on 4/14/14.
  */
-public class PlaceIrrigationTile {
+public class PlaceIrrigationTile extends Action {
+
+
+    /*
+        attributes
+     */
+    private Location placement;
+
+
+    /*
+        constructors
+     */
+    PlaceIrrigationTile(){
+        //Empty constructor
+        //used for loading
+    }
+
+    PlaceIrrigationTile(Location placement){
+        this.placement = placement;
+    }
+
+
+    @Override
+    public ActionResult tryAction(GameModel game) {
+        //todo
+
+     /*
+        Check if the action is valid to complete
+        ...
+        returns true if valid
+                false if invalid
+     */
+        boolean isSuccess = true;
+        int famePoints = 0;
+        int actionPoints = 0;
+        String message = "";
+
+        //Check if the player has enough AP points - 1
+        if(true){
+            isSuccess = isSuccess && true;
+
+        }
+        else{
+            isSuccess = isSuccess && false;
+            message += "Error: You do not have enough AP points.\n";
+        }
+
+        //check if required elevation is 0
+        if(true){
+            isSuccess = isSuccess && true;
+        }
+        else{
+            isSuccess = isSuccess && false;
+            message += "Error: The elevation is nonzero.\n";
+        }
+
+        //checks if the player is placing outside of central java
+        if(true){
+            isSuccess = isSuccess && true;
+        }
+        else{
+            isSuccess = isSuccess && false;
+            message += "Error: This tile cannot be placed outside Central Java.\n";
+        }
+
+        //check if the player would surround a body of water by placing this tile
+        //this would only gain the player fame points.
+
+
+        return new ActionResult(isSuccess, famePoints, actionPoints, message, this);
+    }
+
+    @Override
+    public ActionResult doAction(GameModel game) {
+    /*
+        Check if the action is valid
+        Do the action if is valid to so
+        ...
+     */
+        ActionResult result = tryAction(game);
+        if(result.isSuccess()) {
+
+            //Decrememnt the AP points it cost
+
+            //Award the fame points to the player
+
+            //decrement the number of irrigation tiles from shared resources
+
+            //place the tile on the board
+
+        }
+        return result;
+    }
+
+
+    @Override
+    public String serialize() {
+     /*
+        Formats the attributes of this object
+        into a String that can be used
+        for loading and saving.
+
+        Utilizes Json and JsonObject to accomplish this.
+     */
+
+        return null;
+    }
+
+    @Override
+
+    public Action restore(JsonObject actionToRestore) {
+    /*
+        Converts the JsonObject used when loading
+        to restore the states/attributes of this action.
+
+        Utilized JsonObject to accomplish this
+     */
+        return null;
+    }
 }
--- src/model/actions/tiles/PlacePalaceTile.java
+++ src/model/actions/tiles/PlacePalaceTile.java
@@ -1,7 +1,182 @@
 package model.actions.tiles;
 
+import model.GameModel;
+import model.actions.Action;
+import model.actions.ActionResult;
+import model.actions.serialization.JsonObject;
+import model.board.Location;
+
 /**
- * Created by Baker on 4/14/2014.
+ * Created by idinamenzel on 4/14/14.
  */
-public class PlacePalaceTile {
+public class PlacePalaceTile extends Action {
+
+
+    /*
+        attributes
+     */
+    private int value;
+    private Location placement;
+
+    /*
+        constructors
+     */
+    PlacePalaceTile(){
+        //Empty constructor
+        //used for loading
+    }
+
+    PlacePalaceTile(int value, Location placement){
+        this.value = value;
+        this.placement = placement;
+    }
+
+
+    @Override
+    public ActionResult tryAction(GameModel game) {
+     /*
+        Check if the action is valid to complete
+        ...
+        returns true if valid
+                false if invalid
+     */
+        boolean isSuccess = true;
+        int famePoints = this.value/2;
+        int actionPoints = 1;
+        String message = "";
+
+        //Check if there are any palace tiles of this value in shared resources
+        if(true){
+            isSuccess = isSuccess && true;
+
+        }
+        else{
+            isSuccess = isSuccess && false;
+            message += "Error: There are not enough palaces tiles.\n";
+        }
+
+
+        //Check if the player has enough AP points to complete this - 1 AP
+        //ActionPointsRule
+
+        if(true){
+            isSuccess = isSuccess && true;
+
+        }
+        else{
+            isSuccess = isSuccess && false;
+            message += "Error: You do not have enough AP points.\n";
+        }
+
+
+        //see if you are placing this on top of a village
+        //PalacePlacementRule
+        if(true){
+            isSuccess = isSuccess && true;
+
+            /*
+                Check other rules that only correspond to the placement location being a village
+             */
+
+            //Check if there is not already a palace in that city/village
+            if(true){
+                isSuccess = isSuccess && true;
+
+            }
+            else{
+                isSuccess = isSuccess && false;
+                message += "Error: This is already a city with a palace.\n";
+            }
+
+            //Check if there are enough villages to make it a city
+            if(true){
+                isSuccess = isSuccess && true;
+
+            }
+            else{
+                isSuccess = isSuccess && false;
+                message += "Error: There are not enough villages.\n";
+            }
+
+            //Check if the player is the highest ranked player in this city
+            //HighestRankedPlayerInCityRule
+            if(true){
+                isSuccess = isSuccess && true;
+
+            }
+            else{
+                isSuccess = isSuccess && false;
+                message += "Error: You are not the highest ranked player.\n";
+            }
+
+        }
+        else{
+            isSuccess = isSuccess && false;
+            message += "Error: Palaces need to be built on villages.\n";
+        }
+
+        //Check if the player has enough AP points to complete this - 1 AP
+        //PlaceTileOnDeveloperRule
+        if(true){
+            isSuccess = isSuccess && true;
+
+        }
+        else{
+            isSuccess = isSuccess && false;
+            message += "Error: You cannot place this tile on top of a developer.\n";
+        }
+
+        //todo
+
+        return new ActionResult(isSuccess, famePoints, actionPoints, message, this);
+    }
+
+    @Override
+    public ActionResult doAction(GameModel game) {
+    /*
+        Check if the action is valid
+        Do the action if is valid to so
+        ...
+     */
+        ActionResult result = tryAction(game);
+        if(result.isSuccess()) {
+
+            //Decrememnt the AP point
+
+            //place the palace on this location of the board
+
+            //award the player fame points
+
+            //set this location to palacesinteractedwith in the turn object
+
+            //decremenet the number of this valued palace in the shared resources
+
+        }
+        return result;
+    }
+
+
+    @Override
+    public String serialize() {
+     /*
+        Formats the attributes of this object
+        into a String that can be used
+        for loading and saving.
+
+        Utilizes Json and JsonObject to accomplish this.
+     */
+        return null;
+    }
+
+    @Override
+
+    public PlacePalaceTile restore(JsonObject actionToRestore) {
+    /*
+        Converts the JsonObject used when loading
+        to restore the states/attributes of this action.
+
+        Utilized JsonObject to accomplish this
+     */
+        return null;
+    }
 }
