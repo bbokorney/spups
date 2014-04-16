db723707d00eb4fc2d13293a0f23fdd6e58e6af4
Sachit Mishra <smishra2@ufl.edu>
2014-04-16 01:10:51 -0400

    Added Shared Resources class

--- src/model/sharedresources/Resources.java
+++
@@ -1,7 +0,0 @@
-package model.sharedresources;
-
-/**
- * Created by Baker on 4/14/2014.
- */
-public class Resources {
-}
---
+++ src/model/sharedresources/SharedResources.java
@@ -0,0 +1,7 @@
+package model.sharedresources;
+
+/**
+ * Created by Sachit on 4/16/2014.
+ */
+public class SharedResources {
+}
