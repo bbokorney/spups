f6960a335a59cb690421e4c1161eae82939fa060
Meghan King <meghantking67@gmail.com>
2014-04-15 22:13:07 -0400

    Added Potential(Two/Three)SpaceMovement classes

---
+++ src/model/potentialactions/PotentialThreeSpaceMovement.java
@@ -0,0 +1,118 @@
+package model.potentialactions;
+
+import model.GameModel;
+import model.actions.ActionResult;
+import model.board.HexLocation;
+
+/**
+ * Created by idinamenzel on 4/15/14.
+ */
+public abstract class PotentialThreeSpaceMovement extends PotentialAction implements HexComponentMovement{
+
+
+    private HexLocation centerLocation;
+    private HexLocation[] otherLocations;
+
+    public PotentialThreeSpaceMovement(GameModel game){
+        super(game);
+    }
+
+    @Override
+    public ActionResult moveNorth() {
+        HexLocation newCenterLocation = centerLocation.getNeighbor(0);
+        HexLocation newOtherLocation = otherLocations[0].getNeighbor(0);
+        HexLocation newOtherOtherLocation = otherLocations[1].getNeighbor(0);
+
+        if (game.getBoard().areLocationsOnBoard(newCenterLocation, newOtherLocation, newOtherOtherLocation)) {
+            centerLocation = newCenterLocation;
+            otherLocations[0] = newOtherLocation;
+            otherLocations[1] = newOtherLocation;
+            this.setComponentsOnHoverBoard();
+        }
+        return this.getActionResult();
+    }
+
+    @Override
+    public ActionResult moveNortheast() {
+        HexLocation newCenterLocation = centerLocation.getNeighbor(1);
+        HexLocation newOtherLocation = otherLocations[0].getNeighbor(1);
+        HexLocation newOtherOtherLocation = otherLocations[1].getNeighbor(1);
+
+        if (game.getBoard().areLocationsOnBoard(newCenterLocation, newOtherLocation, newOtherOtherLocation)) {
+            centerLocation = newCenterLocation;
+            otherLocations[0] = newOtherLocation;
+            otherLocations[1] = newOtherLocation;
+            this.setComponentsOnHoverBoard();
+        }
+        return this.getActionResult();
+    }
+
+    @Override
+    public ActionResult moveSoutheast() {
+        HexLocation newCenterLocation = centerLocation.getNeighbor(2);
+        HexLocation newOtherLocation = otherLocations[0].getNeighbor(2);
+        HexLocation newOtherOtherLocation = otherLocations[1].getNeighbor(2);
+
+        if (game.getBoard().areLocationsOnBoard(newCenterLocation, newOtherLocation, newOtherOtherLocation)) {
+            centerLocation = newCenterLocation;
+            otherLocations[0] = newOtherLocation;
+            otherLocations[1] = newOtherLocation;
+            this.setComponentsOnHoverBoard();
+        }
+        return this.getActionResult();
+    }
+
+    @Override
+    public ActionResult moveSouth() {
+        HexLocation newCenterLocation = centerLocation.getNeighbor(3);
+        HexLocation newOtherLocation = otherLocations[0].getNeighbor(3);
+        HexLocation newOtherOtherLocation = otherLocations[1].getNeighbor(3);
+
+        if (game.getBoard().areLocationsOnBoard(newCenterLocation, newOtherLocation, newOtherOtherLocation)) {
+            centerLocation = newCenterLocation;
+            otherLocations[0] = newOtherLocation;
+            otherLocations[1] = newOtherLocation;
+            this.setComponentsOnHoverBoard();
+        }
+        return this.getActionResult();
+    }
+
+    @Override
+    public ActionResult moveSouthwest() {
+        HexLocation newCenterLocation = centerLocation.getNeighbor(4);
+        HexLocation newOtherLocation = otherLocations[0].getNeighbor(4);
+        HexLocation newOtherOtherLocation = otherLocations[1].getNeighbor(4);
+
+        if (game.getBoard().areLocationsOnBoard(newCenterLocation, newOtherLocation, newOtherOtherLocation)) {
+            centerLocation = newCenterLocation;
+            otherLocations[0] = newOtherLocation;
+            otherLocations[1] = newOtherLocation;
+            this.setComponentsOnHoverBoard();
+        }
+        return this.getActionResult();
+    }
+
+
+    @Override
+    public ActionResult moveNorthwest() {
+        HexLocation newCenterLocation = centerLocation.getNeighbor(5);
+        HexLocation newOtherLocation = otherLocations[0].getNeighbor(5);
+        HexLocation newOtherOtherLocation = otherLocations[1].getNeighbor(5);
+
+        if (game.getBoard().areLocationsOnBoard(newCenterLocation, newOtherLocation, newOtherOtherLocation)) {
+            centerLocation = newCenterLocation;
+            otherLocations[0] = newOtherLocation;
+            otherLocations[1] = newOtherLocation;
+            this.setComponentsOnHoverBoard();
+        }
+        return this.getActionResult();
+    }
+
+    public HexLocation getCenterLocation(){
+        return centerLocation;
+    }
+
+    public HexLocation[] getOtherLocations(){
+        return otherLocations;
+    }
+}
---
+++ src/model/potentialactions/PotentialTwoSpaceMovement.java
@@ -0,0 +1,103 @@
+package model.potentialactions;
+
+import model.GameModel;
+import model.actions.ActionResult;
+import model.board.HexLocation;
+
+/**
+ * Created by idinamenzel on 4/15/14.
+ */
+public abstract class PotentialTwoSpaceMovement extends PotentialAction implements HexComponentMovement {
+
+    private HexLocation centerLocation;
+    private HexLocation otherLocation;
+
+    public PotentialTwoSpaceMovement(GameModel game){
+        super(game);
+    }
+
+    @Override
+    public ActionResult moveNorth() {
+        HexLocation newCenterLocation = centerLocation.getNeighbor(0);
+        HexLocation newOtherLocation = otherLocation.getNeighbor(0);
+        if (game.getBoard().areLocationsOnBoard(newCenterLocation, newOtherLocation)) {
+            centerLocation = newCenterLocation;
+            otherLocation = newOtherLocation;
+            this.setComponentsOnHoverBoard();
+        }
+        return this.getActionResult();
+    }
+
+    @Override
+    public ActionResult moveNortheast() {
+        HexLocation newCenterLocation = centerLocation.getNeighbor(1);
+        HexLocation newOtherLocation = otherLocation.getNeighbor(1);
+        if (game.getBoard().areLocationsOnBoard(newCenterLocation, newOtherLocation)) {
+            centerLocation = newCenterLocation;
+            otherLocation = newOtherLocation;
+            this.setComponentsOnHoverBoard();
+        }
+        return this.getActionResult();
+    }
+
+    @Override
+    public ActionResult moveSoutheast() {
+        HexLocation newCenterLocation = centerLocation.getNeighbor(2);
+        HexLocation newOtherLocation = otherLocation.getNeighbor(2);
+        if (game.getBoard().areLocationsOnBoard(newCenterLocation, newOtherLocation)) {
+            centerLocation = newCenterLocation;
+            otherLocation = newOtherLocation;
+            this.setComponentsOnHoverBoard();
+        }
+        return this.getActionResult();
+    }
+
+    @Override
+    public ActionResult moveSouth() {
+        HexLocation newCenterLocation = centerLocation.getNeighbor(3);
+        HexLocation newOtherLocation = otherLocation.getNeighbor(3);
+        if (game.getBoard().areLocationsOnBoard(newCenterLocation, newOtherLocation)) {
+            centerLocation = newCenterLocation;
+            otherLocation = newOtherLocation;
+            this.setComponentsOnHoverBoard();
+        }
+        return this.getActionResult();
+    }
+
+    @Override
+    public ActionResult moveSouthwest() {
+        HexLocation newCenterLocation = centerLocation.getNeighbor(4);
+        HexLocation newOtherLocation = otherLocation.getNeighbor(4);
+        if (game.getBoard().areLocationsOnBoard(newCenterLocation, newOtherLocation)) {
+            centerLocation = newCenterLocation;
+            otherLocation = newOtherLocation;
+            this.setComponentsOnHoverBoard();
+        }
+        return this.getActionResult();
+    }
+
+
+    @Override
+    public ActionResult moveNorthwest() {
+        HexLocation newCenterLocation = centerLocation.getNeighbor(5);
+        HexLocation newOtherLocation = otherLocation.getNeighbor(5);
+        if (game.getBoard().areLocationsOnBoard(newCenterLocation, newOtherLocation)) {
+            centerLocation = newCenterLocation;
+            otherLocation = newOtherLocation;
+            this.setComponentsOnHoverBoard();
+        }
+        return this.getActionResult();
+    }
+
+    public HexLocation getCenterLocation(){
+        return centerLocation;
+    }
+
+    public HexLocation getOtherLocation(){
+        return otherLocation;
+    }
+
+
+
+
+}
