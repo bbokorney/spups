7da1c0ab20c91989789d38cef644ea847565cbb8
Sachit Mishra <smishra2@ufl.edu>
2014-04-15 21:55:17 -0400

    Tile got delete for some reason

--- src/model/tiles/Tile.java
+++ src/model/tiles/Tile.java
@@ -4,4 +4,9 @@ package model.tiles;
  * Created by Baker on 4/14/2014.
  */
 public class Tile {
+    private int numberOfComponents;
+
+    public int getNumberOfComponents() {
+        return numberOfComponents;
+    }
 }
