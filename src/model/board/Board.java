05441d8e0a9a82218a6a481e6e83e0230bc25fe5
Baker Bokorney <bbokorney@gmail.com>
2014-04-15 22:44:28 -0400

    Added some getters.

--- src/model/board/Board.java
+++ src/model/board/Board.java
@@ -11,25 +11,38 @@ import model.tiles.TileComponent;
 public abstract class Board {
 
 	private Map<Location, Space> board;
+
     private BodyOfWaterContainer bodyOfWaterContainer;
+
     private CityContainer cityContainer;
     private VillageContainer villageContainer;
-	
 	public Board() {
 		board = new HashMap<Location, Space>();
         bodyOfWaterContainer = new BodyOfWaterContainer();
         cityContainer = new CityContainer();
         villageContainer = new VillageContainer();
 	}
-	
-	public void placeSpace(Location loc, Space space) {
+
+    public BodyOfWaterContainer getBodyOfWaterContainer() {
+        return bodyOfWaterContainer;
+    }
+
+    public CityContainer getCityContainer() {
+        return cityContainer;
+    }
+
+    public VillageContainer getVillageContainer() {
+        return villageContainer;
+    }
+
+    public void placeSpace(Location loc, Space space) {
 		board.put(loc, space);
 	}
-	
+
 	public Space getSpace(Location loc) {
 		return board.get(loc);
 	}
-	
+
 	public void placeTopTileComponent(Location loc, TileComponent tile) {
 		Space space = board.get(loc);
 		space.accept(tile);
