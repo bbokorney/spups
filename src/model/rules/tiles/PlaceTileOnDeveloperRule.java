70c09a20a4f35e3aad2fd1aa52580a246d8fa441
Baker Bokorney <bbokorney@gmail.com>
2014-04-18 17:58:26 -0400

    Removed unused paramater.

--- src/model/rules/tiles/PlaceTileOnDeveloperRule.java
+++ src/model/rules/tiles/PlaceTileOnDeveloperRule.java
@@ -11,7 +11,7 @@ import model.tiles.TileComponent;
  * Created by Baker on 4/14/2014.
  */
 public class PlaceTileOnDeveloperRule {
-    public static boolean canPlaceTile(Location location, TileComponent component, GameModel model) {
+    public static boolean canPlaceTile(Location location, GameModel model) {
         for(Developer dev : model.getDevelopers()) {
             if(dev.getLocation().equals(location)) {
                 return false;
