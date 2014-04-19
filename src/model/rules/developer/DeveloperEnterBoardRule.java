de07fdc738b1156ae825c0889b29052e29b589fa
Baker Bokorney <bbokorney@gmail.com>
2014-04-18 20:13:20 -0400

    Developer enter and exit board rules.

--- src/model/rules/developer/DeveloperEnterBoardRule.java
+++ src/model/rules/developer/DeveloperEnterBoardRule.java
@@ -1,12 +1,13 @@
 package model.rules.developer;
 
+import model.board.BoardRuleHelper;
 import model.board.Location;
 
 /**
  * Created by Baker on 4/14/2014.
  */
 public class DeveloperEnterBoardRule {
-    public static boolean canEnterHere(Location location) {
-        return false;
+    public static boolean canEnterHere(Location location, BoardRuleHelper helper) {
+        return  helper.developerCanEnterHere(location);
     }
 }
--- src/model/rules/developer/DeverloperExitBoardRule.java
+++ src/model/rules/developer/DeverloperExitBoardRule.java
@@ -1,12 +1,13 @@
 package model.rules.developer;
 
+import model.board.BoardRuleHelper;
 import model.board.Location;
 
 /**
  * Created by Baker on 4/14/2014.
  */
 public class DeverloperExitBoardRule {
-    public static boolean canExitHere(Location location) {
-        return false;
+    public static boolean canExitHere(Location location, BoardRuleHelper helper) {
+        return helper.developerCanBeRemovedFromHere(location);
     }
 }
