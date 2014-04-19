1c7ab13345abf9ad6c3b7748aaaf87e88cf3c6e6
Baker Bokorney <bbokorney@gmail.com>
2014-04-19 19:47:02 -0400

    Changed interface of Palace placement rule.

--- src/model/rules/tiles/PalacePlacementRule.java
+++ src/model/rules/tiles/PalacePlacementRule.java
@@ -1,5 +1,7 @@
 package model.rules.tiles;
 
+import model.board.Board;
+import model.board.Location;
 import model.palacefestival.PalaceCardComponent;
 import model.tiles.IrrigationTileComponent;
 import model.tiles.PalaceTileComponent;
@@ -9,28 +11,61 @@ import model.tiles.VillageTileComponent;
 /**
  * Created by Baker on 4/14/2014.
  */
-public class PalacePlacementRule implements Visitor {
+public class PalacePlacementRule {
     private boolean allowed;
+    private Location location;
+    private Board board;
 
-    public PalacePlacementRule() {
+    public PalacePlacementRule(Location location, Board board) {
         allowed = false;
+        this.location = location;
+        this.board = board;
     }
 
-    public boolean allowed() { return allowed; }
-
-    public void visit(VillageTileComponent component) {
-        allowed = true;
+    public boolean buildAllowed() {
+        board.getSpace(location).getTopTileComponent().accept(new BuildVisitor());
+        return allowed;
     }
 
-    public void visit(RiceTileComponent component) {
-        allowed = false;
+    public boolean upgradeAllowed() {
+        board.getSpace(location).getTopTileComponent().accept(new UpgradeVisitor());
+        return allowed;
     }
 
-    public void visit(PalaceTileComponent component) {
-        allowed = false;
+    private class BuildVisitor implements Visitor {
+
+        public void visit(VillageTileComponent component) {
+            allowed = true;
+        }
+
+        public void visit(RiceTileComponent component) {
+            allowed = false;
+        }
+
+        public void visit(PalaceTileComponent component) {
+            allowed = false;
+        }
+
+        public void visit(IrrigationTileComponent component) {
+            allowed = false;
+        }
     }
 
-    public void visit(IrrigationTileComponent component) {
-        allowed = false;
+    private class UpgradeVisitor implements Visitor {
+        public void visit(VillageTileComponent component) {
+            allowed = true;
+        }
+
+        public void visit(RiceTileComponent component) {
+            allowed = false;
+        }
+
+        public void visit(PalaceTileComponent component) {
+            allowed = true;
+        }
+
+        public void visit(IrrigationTileComponent component) {
+            allowed = false;
+        }
     }
 }
