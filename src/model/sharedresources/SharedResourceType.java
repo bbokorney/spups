b487fc4708e5b5b8892ea49c8c7237708894d78f
Sachit Mishra <smishra2@ufl.edu>
2014-04-18 21:26:45 -0400

    Added shared resource type enum

--- src/model/sharedresources/SharedResourceType.java
+++ src/model/sharedresources/SharedResourceType.java
@@ -4,4 +4,6 @@ package model.sharedresources;
  * Created by Baker on 4/14/2014.
  */
 public enum SharedResourceType {
+    IRRIGATION,THREE, PALACELEVELTWO, PALACELEVELFOUR, PALACELEVELSIX,
+    PALACELEVELEIGHT, PALACELEVELTEN
 }
