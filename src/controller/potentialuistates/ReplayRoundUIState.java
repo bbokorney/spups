4162aab8baee4350b3e7621c4adcf873aeee135f
unknown <jonathan.howe.yu@gmail.com>
2014-04-20 02:03:18 -0400

    Filled in methods

--- src/controller/potentialuistates/ReplayRoundUIState.java
+++ src/controller/potentialuistates/ReplayRoundUIState.java
@@ -2,14 +2,24 @@ package controller.potentialuistates;
 
 import controller.Controller;
 import controller.history.TimeTraveler;
+import controller.keylistener.Funktor;
+import controller.keylistener.InternalListener;
 import controller.keylistener.KeyListener;
 import model.GameModel;
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Baker on 4/14/2014.
  */
 public class ReplayRoundUIState {
-	private final int KEY_NEXT
+	private final int KEY_NEXT = KeyEvent.VK_RIGHT;
+	private final int KEY_BACK = KeyEvent.VK_LEFT;
+	private final int KEY_TIMED = KeyEvent.VK_TAB;
+
+	private final long TIME_TO_WAIT = 3000; // Milliseconds
 
 	Controller controller;
 	KeyListener keyListener;
@@ -21,7 +31,52 @@ public class ReplayRoundUIState {
 		this.keyListener = keyListener;
 		this.model = model;
 		this.timeTraveler = timeTraveler;
+
+		initListeners();
 	}
 
+	public void replayNextMove() {
+		if(timeTraveler.hasNext()) timeTraveler.next();
+	}
 
+	public void unReplayMove() {
+		timeTraveler.back();
+	}
+
+	public void replayRoundTimed() {
+		while(timeTraveler.hasNext()) {
+			try { Thread.sleep(TIME_TO_WAIT); }
+			catch(InterruptedException e) { e.printStackTrace(); }
+			timeTraveler.next();
+		}
+	}
+
+	private void initListeners() {
+		List<InternalListener> listeners = new ArrayList<InternalListener>();
+		InternalListener i = new InternalListener(KEY_NEXT, new Funktor() {
+			@Override
+			public void call() {
+				replayNextMove();
+			}
+		});
+		listeners.add(i);
+
+		i = new InternalListener(KEY_BACK, new Funktor() {
+			@Override
+			public void call() {
+				unReplayMove();
+			}
+		});
+		listeners.add(i);
+
+		i = new InternalListener(KEY_TIMED, new Funktor() {
+			@Override
+			public void call() {
+				replayRoundTimed();
+			}
+		});
+		listeners.add(i);
+
+		keyListener.replaceTemporaryListener(listeners);
+	}
 }
