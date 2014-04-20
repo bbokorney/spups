0cd3094b9609df0f5b83baae5f3cec6c9bd89f31
Sachit Mishra <smishra2@ufl.edu>
2014-04-19 23:14:41 -0400

    Added separate tile component place methods for different tile types

--- src/model/board/Board.java
+++ src/model/board/Board.java
@@ -69,11 +69,36 @@ public abstract class Board {
 		Space space = board.get(loc);
 		space.accept(tile);
 	}
+
+    //TODO:
+        /*
+        for Village tile, check
+        1) is it joining two (or more) villages
+        2) is it its own village
+        3) is it joining a village and a city
+        for irrigation, check
+        1) its own body of water?
+        2) a new body of water?
+        for rice, check
+        1) are we splitting a village or city?
+     */
+
+    public void placeIrrigationTileComponent(Location loc, TileComponent tile) {
+
+    }
+
+    public void placeRiceTileComponent(Location loc, TileComponent tile) {
+
+    }
+
+    public void placeVillageTileComponent(Location loc, TileComponent tile) {
+
+    }
 	
 	public TileComponent getTopTileComponent(Location loc) {
 		Space space = board.get(loc);
 		return space.getTopTileComponent();
-	}
+    }
 
     public boolean areLocationsOnBoard(Location... locations){
         //Check that board contains each of these locations
