2aeae8da409b692285a25eae48400f7e8caf6cde
sararose <sara.rose.schuster@gmail.com>
2014-04-20 00:11:01 -0400

    PalaceFestival tie stuff

---
+++ src/model/actions/palacefestival/RequestTie.java
@@ -0,0 +1,66 @@
+package model.actions.palacefestival;
+
+import model.GameModel;
+import model.actions.Action;
+import model.actions.ActionResult;
+import model.actions.serialization.JsonObject;
+import model.palacefestival.PalaceFestivalPlayer;
+
+import java.util.Collection;
+
+/**
+ * Created by Baker on 4/14/2014.
+ */
+public class RequestTie extends Action {
+
+    @Override
+    public ActionResult tryAction(GameModel game) {
+        Collection<PalaceFestivalPlayer> players = game.getFestivalPlayers();
+        int score = -1;
+        boolean tie = true;
+        for (PalaceFestivalPlayer player : players) {
+            int playerScore = player.getScore();
+            if(score == -1) {
+                score = playerScore;
+            }
+            else if (playerScore != score) {
+                tie = false;
+                break;
+            }
+        }
+
+        String message = tie ? "success" : "players are not tied";
+
+        // TODO: send the FP they would win if the tie went through
+        return new ActionResult(tie, 0,0, message, this);
+    }
+
+    @Override
+    public ActionResult doAction(GameModel game) {
+        ActionResult result = tryAction(game);
+        if (result.isSuccess()) {
+            game.advancePalaceFestivalTurn();
+        }
+
+        return result;
+    }
+
+    @Override
+    public String serialize() {
+        // TODO: Sara
+        throw new UnsupportedOperationException("Tell Sara to implement me!");
+    }
+
+    @Override
+    public Action restore(JsonObject actionToRestore) {
+        // TODO: Sara
+        throw new UnsupportedOperationException("Tell Sara to implement me!");
+    }
+
+    @Override
+    public int getActionID() {
+        // TODO: Sara
+        throw new UnsupportedOperationException("Tell Sara to implement me!");
+    }
+
+}
--- src/model/potentialactions/PotentialMoveToTie.java
+++ src/model/potentialactions/PotentialMoveToTie.java
@@ -1,23 +1,38 @@
 package model.potentialactions;
 
 import model.actions.ActionResult;
+import model.actions.palacefestival.AgreeToTie;
+import model.actions.palacefestival.EndPalaceFestival;
+import model.actions.palacefestival.RequestTie;
 import model.palacefestival.PalaceFestivalPlayer;
 
 /**
  * Created by Baker on 4/14/2014.
  */
-public class PotentialMoveToTie {
+public class PotentialMoveToTie extends PotentialAction {
 
-    private PalaceFestivalPlayer playerWhoRequestedTie;
+    private PalaceFestivalPlayer playerWhoRequestedTie = null;
 
-    public ActionResult tieRequested() {
-        // TODO:Sara
-        throw new UnsupportedOperationException("Tell Sara to implement me!");
+    private ActionResult tieRequested() {
+        playerWhoRequestedTie = getGameModel().getCurrentPalaceFestivalPlayer();
+        return new RequestTie().tryAction(getGameModel());
     }
 
-    public ActionResult tieAgreed() {
-        // TODO:Sara
-        throw new UnsupportedOperationException("Tell Sara to implement me!");
+    private ActionResult tieAgreed() {
+        ActionResult tieActionResult = new AgreeToTie(playerWhoRequestedTie).tryAction(getGameModel());
+        if (!tieActionResult.isSuccess() || !getGameModel().getCurrentPalaceFestivalPlayer().equals(playerWhoRequestedTie))
+            return tieActionResult;
+
+        return new EndPalaceFestival().tryAction(getGameModel());
     }
 
+    @Override
+    protected void setComponentsOnHoverBoard() {
+        // nothing to set
+    }
+
+    @Override
+    protected ActionResult getActionResult() {
+        return playerWhoRequestedTie == null ? tieRequested() : tieAgreed();
+    }
 }
