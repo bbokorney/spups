8a9b7f428447582fb71c37acd306b4ffb4acfe78
sararose <sara.rose.schuster@gmail.com>
2014-04-21 03:11:36 -0400

    fuck bitches get $$$$

--- src/controller/potentialuistates/BeginPalaceFestivalTurnUIState.java
+++ src/controller/potentialuistates/BeginPalaceFestivalTurnUIState.java
@@ -1,7 +1,91 @@
 package controller.potentialuistates;
 
+import controller.Controller;
+import controller.keylistener.Funktor;
+import controller.keylistener.InternalListener;
+import controller.keylistener.KeyListener;
+import model.GameModel;
+import model.potentialactions.PotentialBeginPalaceFestival;
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Baker on 4/14/2014.
  */
 public class BeginPalaceFestivalTurnUIState {
+
+    private final int KEY_TAB = KeyEvent.VK_TAB;
+    private final int KEY_CANCEL = KeyEvent.VK_ESCAPE;
+    private final int KEY_CANCEL_CARD = KeyEvent.VK_DELETE;
+    private final int KEY_CONFIRM = KeyEvent.VK_ENTER;
+    private final int KEY_LEFT = KeyEvent.VK_LEFT;
+    private final int KEY_RIGHT = KeyEvent.VK_RIGHT;
+
+    Controller controller;
+    KeyListener keyListener;
+    GameModel model;
+
+    PotentialBeginPalaceFestival potentialAction;
+
+    public BeginPalaceFestivalTurnUIState(Controller controller, KeyListener keyListener, GameModel model){
+        this.controller = controller;
+        this.keyListener = keyListener;
+        this.model = model;
+
+        potentialAction = new PotentialBeginPalaceFestival();
+
+        initListeners();
+    }
+
+    private void initListeners() {
+
+        List<InternalListener> listeners = new ArrayList<InternalListener>();
+        listeners.add( new InternalListener(KEY_TAB, new Funktor() {
+            @Override
+            public void call() {
+                potentialAction.tabToNextPalace();
+            }
+        }));
+
+        listeners.add( new InternalListener(KEY_CANCEL, new Funktor() {
+            @Override
+            public void call() {
+                //todo figure this out
+            }
+        }));
+
+        listeners.add( new InternalListener(KEY_CANCEL_CARD, new Funktor() {
+            @Override
+            public void call() {
+                potentialAction.removeCurrentCardFromBid();
+            }
+        }));
+
+        listeners.add( new InternalListener(KEY_CONFIRM, new Funktor() {
+            @Override
+            public void call() {
+                potentialAction.chooseCurrentCard();
+            }
+        }));
+
+        listeners.add( new InternalListener(KEY_LEFT, new Funktor() {
+            @Override
+            public void call() {
+                potentialAction.tabToNextCard();
+            }
+        }));
+
+        listeners.add( new InternalListener(KEY_RIGHT, new Funktor() {
+            @Override
+            public void call() {
+                potentialAction.tabToPreviousCard();
+            }
+        }));
+
+        keyListener.replaceTemporaryListener(listeners);
+
+    }
+
 }
---
+++ src/controller/potentialuistates/PlaceDeveloperOnBoardUIState.java
@@ -0,0 +1,157 @@
+package controller.potentialuistates;
+
+import controller.Controller;
+import controller.keylistener.Funktor;
+import controller.keylistener.InternalListener;
+import controller.keylistener.KeyListener;
+import model.GameModel;
+import model.Pair;
+import model.actions.ActionResult;
+import model.actions.developer.PlaceDeveloperOnBoard;
+import model.potentialactions.PotentialPlaceDeveloperOnBoard;
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Baker on 4/14/2014.
+ */
+public class PlaceDeveloperOnBoardUIState extends GameplayUIState {
+    private final int KEY_NORTH = KeyEvent.VK_NUMPAD8;
+    private final int KEY_SOUTH = KeyEvent.VK_NUMPAD2;
+    private final int KEY_NORTHEAST = KeyEvent.VK_NUMPAD9;
+    private final int KEY_SOUTHEAST = KeyEvent.VK_NUMPAD3;
+    private final int KEY_NORTHWEST = KeyEvent.VK_NUMPAD7;
+    private final int KEY_SOUTHWEST = KeyEvent.VK_NUMPAD1;
+
+    private final int KEY_CONFIRM = KeyEvent.VK_ENTER;
+
+    Controller controller;
+    KeyListener keyListener;
+    GameModel model;
+
+    PotentialPlaceDeveloperOnBoard potentialAction;
+
+    public PlaceDeveloperOnBoardUIState(Controller controller, KeyListener keyListener, GameModel model){
+        this.controller = controller;
+        this.keyListener = keyListener;
+        this.model = model;
+
+        potentialAction = new PotentialPlaceDeveloperOnBoard(model);
+
+        initListeners();
+    }
+
+    public void moveNorth() {
+        ActionResult result = potentialAction.moveNorth();
+        if(!result.isSuccess()) {
+
+        }
+    }
+
+    public void moveSouth() {
+        ActionResult result = potentialAction.moveSouth();
+        if(!result.isSuccess()) {
+
+        }
+    }
+
+    public void moveNortheast() {
+        ActionResult result = potentialAction.moveNortheast();
+        if(!result.isSuccess()) {
+
+        }
+    }
+
+    public void moveNorthwest() {
+        ActionResult result = potentialAction.moveNorthwest();
+        if(!result.isSuccess()) {
+
+        }
+    }
+
+    public void moveSoutheast() {
+        ActionResult result = potentialAction.moveSoutheast();
+        if(!result.isSuccess()) {
+
+        }
+    }
+
+    public void moveSouthwest() {
+        ActionResult result = potentialAction.moveSouthwest();
+        if(!result.isSuccess()) {
+
+        }
+    }
+
+    public void confirmPlacement() {
+        Pair<ActionResult, PlaceDeveloperOnBoard> actionPair = potentialAction.confirmMovement();
+        ActionResult result = actionPair.getFirst();
+
+        if(result.isSuccess()) {
+            controller.addToHistory(actionPair);
+        }
+    }
+
+    private void initListeners() {
+        List<InternalListener> listeners = new ArrayList<InternalListener>();
+        InternalListener i = new InternalListener(KEY_NORTH, new Funktor() {
+            @Override
+            public void call() {
+                moveNorth();
+            }
+        });
+        listeners.add(i);
+
+        i = new InternalListener(KEY_SOUTH, new Funktor() {
+            @Override
+            public void call() {
+                moveSouth();
+            }
+        });
+        listeners.add(i);
+
+        i = new InternalListener(KEY_NORTHWEST, new Funktor() {
+            @Override
+            public void call() {
+                moveNorthwest();
+            }
+        });
+        listeners.add(i);
+
+        i = new InternalListener(KEY_NORTHEAST, new Funktor() {
+            @Override
+            public void call() {
+                moveNortheast();
+            }
+        });
+        listeners.add(i);
+
+        i = new InternalListener(KEY_SOUTHWEST, new Funktor() {
+            @Override
+            public void call() {
+                moveSoutheast();
+            }
+        });
+        listeners.add(i);
+
+        i = new InternalListener(KEY_SOUTHEAST, new Funktor() {
+            @Override
+            public void call() {
+                moveSouthwest();
+            }
+        });
+        listeners.add(i);
+
+        i = new InternalListener(KEY_CONFIRM, new Funktor() {
+            @Override
+            public void call() {
+                confirmPlacement();
+            }
+        });
+        listeners.add(i);
+
+        keyListener.replaceTemporaryListener(listeners);
+    }
+}
