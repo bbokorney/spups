8a89349b29b263f6ec3917cce894fbcaec300437
Meghan King <meghantking67@gmail.com>
2014-04-21 00:25:22 -0400

    Added null checking in Tile Rules

--- src/model/rules/tiles/PlacementOnSameSizeTileRule.java
+++ src/model/rules/tiles/PlacementOnSameSizeTileRule.java
@@ -9,13 +9,16 @@ import model.tiles.Tile;
  */
 public class PlacementOnSameSizeTileRule {
     public static boolean placingOnSameTile(Board board, Location... locations) {
-        Tile tile = board.getTopTileComponent(locations[0]).getParent();
-        int count  = 1;
-        for(int i = 1; i < locations.length; ++i) {
-            if(tile.equals(board.getTopTileComponent(locations[i]))) {
-                ++count;
+        if(board.getTopTileComponent(locations[0]) != null) {
+            Tile tile = board.getTopTileComponent(locations[0]).getParent();
+            int count = 1;
+            for (int i = 1; i < locations.length; ++i) {
+                if (tile.equals(board.getTopTileComponent(locations[i]))) {
+                    ++count;
+                }
             }
+            return count == tile.getNumberOfComponents();
         }
-        return count == tile.getNumberOfComponents();
+        return true;
     }
 }
