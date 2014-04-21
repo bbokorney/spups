9c1d682931ca4798ea73e657789371ffbc5e6e4c
Meghan King <meghantking67@gmail.com>
2014-04-20 21:43:00 -0400

    Added comment to remind about planning and play mode

--- src/model/actions/EndTurn.java
+++ src/model/actions/EndTurn.java
@@ -38,6 +38,8 @@ public class EndTurn extends Action {
 
         //Check if the turn can advance
 
+        //todo talk to controller about mapping key press for end turn to NOT WORK which forces them to go between planning and play mode
+
         if(!game.canAdvanceJavaTurn()){
             isSuccess = false;
             message += "Player cannot end their turn";
