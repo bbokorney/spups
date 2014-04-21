6089f1295b54c6aa32c27c22a341b8c36602d5df
Baker Bokorney <bbokorney@gmail.com>
2014-04-21 03:34:15 -0400

    Fixed a bug where the palaces could not be placed on villages.

--- src/model/rules/palace/PalaceLevelCitySizeRule.java
+++ src/model/rules/palace/PalaceLevelCitySizeRule.java
@@ -3,6 +3,7 @@ package model.rules.palace;
 import model.board.Board;
 import model.board.City;
 import model.board.Location;
+import model.board.Village;
 
 import java.util.Collection;
 
@@ -14,8 +15,14 @@ public class PalaceLevelCitySizeRule {
         Collection<City> cities = board.getCityContainer().getCityCollection();
         // find the city this location is part of
         for(City city : cities) {
-            if(city.getCity().contains(location) && city.getCity().size() > palaceLevel) {
+            if(city.getCity().contains(location) && city.getCity().size() >= palaceLevel) {
                 return true;
+            }
+        }
+        for(Village village : board.getVillageContainer().getVillages()) {
+            if(village.getLocations().contains(location) &&
+                    village.getLocations().size() >= palaceLevel) {
+
             }
         }
         return false;
