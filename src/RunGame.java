902431a58da0ee7284fe577c3b88081338ba9537
Mauricio E. Bell'Albero <mauriciofmar3@gmail.com>
2014-04-21 00:47:30 -0400

    RunGame test

--- src/RunGame.java
+++ src/RunGame.java
@@ -1,9 +1,15 @@
+import java.util.Stack;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
 
 import model.GameModel;
 import model.JavaGameModel;
+import model.palacefestival.Card;
+import model.palacefestival.PalaceCard;
+import model.palacefestival.PalaceCardComponent;
+import model.palacefestival.PalaceFestival;
 import controller.Controller;
 import view.GameFrame;
 
@@ -24,26 +30,52 @@ public class RunGame {
 				SwingUtilities.invokeLater(new Runnable(){
 		            public void run(){
 		            	GameModel model = new JavaGameModel(3);
+		                PalaceFestival festival = createPalaceFestival();
 		                GameFrame frame = new GameFrame();
 		                @SuppressWarnings("unused")
 		                Controller controller = new Controller(frame);
 		                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		                frame.setVisible(true);
+		                
+		                frame.refreshGame(model,festival,null,null,null);
+		                
 		            }
 		        });
 					return null;
 			}
 		};
 		worker.execute();
-//        SwingUtilities.invokeLater(new Runnable(){
-//            public void run(){
-//            	GameModel model = new JavaGameModel(3);
-//                GameFrame frame = new GameFrame(model);
-//                @SuppressWarnings("unused")
-//                Controller controller = new Controller(frame);
-//                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//                frame.setVisible(true);
-//            }
-//        });
     }    
+    
+    public PalaceFestival createPalaceFestival() { 
+    	 Stack<Card> deck = new Stack<Card>();
+         for (int i = 0; i < 5; i++) {
+             PalaceCard card = new PalaceCard(PalaceCardComponent.DRUM);
+             deck.add(card);
+         }
+         for (int i = 0; i < 5; i++) {
+             PalaceCard card = new PalaceCard(PalaceCardComponent.MASK);
+             deck.add(card);
+         }
+         for (int i = 0; i < 5; i++) {
+             PalaceCard card = new PalaceCard(PalaceCardComponent.PUPPET);
+             deck.add(card);
+         }
+         for (int i = 0; i < 5; i++) {
+             PalaceCard card = new PalaceCard(PalaceCardComponent.MASK,
+                     PalaceCardComponent.PUPPET);
+             deck.add(card);
+         }
+         for (int i = 0; i < 5; i++) {
+             PalaceCard card = new PalaceCard(PalaceCardComponent.DRUM,
+                     PalaceCardComponent.MASK);
+             deck.add(card);
+         }
+         for (int i = 0; i < 5; i++) {
+             PalaceCard card = new PalaceCard(PalaceCardComponent.DRUM,
+                     PalaceCardComponent.PUPPET);
+             deck.add(card);
+         }
+         return new PalaceFestival(null, deck);
+    }
 }
