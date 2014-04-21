8ec944a575f2e008c9d873b96ff62a669ee492f2
Sachit Mishra <smishra2@ufl.edu>
2014-04-20 20:03:18 -0400

    Added getVillageFromLocation(loc) to villageContainer

--- src/model/board/VillageContainer.java
+++ src/model/board/VillageContainer.java
@@ -26,4 +26,13 @@ public class VillageContainer {
             villageCollection.remove(village);
     }
 
+    //Returns the village in which this location exists. If location
+    //is not in a village, return null.
+    public Village getVillageFromLocation(Location loc) {
+        for (Village village : villageCollection)
+            if (village.getLocations().contains(loc))
+                return village;
+        return null;
+    }
+
 }
